d756dbe3ab432285fde756f74006304988d9e2bb
Dadan06 <nyavoandrianony@gmail.com>
2019-10-05 14:48:36 +0300

    [PRODUCT-FORM] fix linting error #master

--- src/app/product/components/product-form/product-form.component.ts
+++ src/app/product/components/product-form/product-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.sass']
+    selector: 'app-product-form',
+    templateUrl: './product-form.component.html',
+    styleUrls: ['./product-form.component.sass']
 })
 export class ProductFormComponent implements OnInit {
+    constructor() {
+        /** */
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        /** */
+    }
 }
