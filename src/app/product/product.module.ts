8e716032836b6ae66f90f26c94fe1559833c65bd
Dadan06 <nyavoandrianony@gmail.com>
2019-10-05 14:41:05 +0300

    [PRODUCT-FORM] Generate component #master

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
-  declarations: [ProductListComponent],
+  declarations: [ProductListComponent, ProductFormComponent],
   imports: [
     CommonModule
   ]
