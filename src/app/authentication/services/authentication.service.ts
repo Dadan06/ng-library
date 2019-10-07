6a806cb17d31e705b1179d92a47073890b7e310f
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 18:41:30 +0300

    [AUTHENTICATION] Back endpoint update #master

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -16,7 +16,7 @@ export class AuthenticationService implements AuthenticationServiceInterface {
 
     logIn(credentials: Credentials): Observable<AuthenticationResponse> {
         return this.http
-            .post(`${environment.apiBaseUrl}/login`, credentials)
+            .post(`${environment.apiBaseUrl}/authentication/login`, credentials)
             .pipe(map((response: ApiResponse) => response.data as AuthenticationResponse));
     }
 }
