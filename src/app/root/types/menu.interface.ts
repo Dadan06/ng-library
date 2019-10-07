f526101313636d37f230cc6d0c8013963b6dcb77
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 18:57:49 +0300

    Remove submenu type

--- src/app/root/types/menu.interface.ts
+++ src/app/root/types/menu.interface.ts
@@ -2,15 +2,9 @@ import { UserPrivileges } from 'src/app/role/constants/privilege.constants';
 
 export interface Menu {
     title: string;
-    icon: string;
+    icon?: string;
     iconActive?: string;
     routerLink?: string;
-    children?: SubMenu[];
-    accessRight?: UserPrivileges;
-}
-
-export interface SubMenu {
-    title: string;
-    routerLink: string;
+    children?: Menu[];
     accessRight?: UserPrivileges;
 }
