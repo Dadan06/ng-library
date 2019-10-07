0cb3b78f283006c8a741c9fe022abc4e7432fe9d
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 21:04:52 +0300

    [HEADER] Menus update #master

--- src/app/root/constants/header-menu.interface.ts
+++ src/app/root/constants/header-menu.interface.ts
@@ -1,26 +1,22 @@
-import { UserPrivileges } from 'src/app/role/constants/privilege.constants';
 import { Menu } from '../types/menu.interface';
 
 export const HEADER_MENU: Menu[] = [
     {
-        title: 'Acceuil',
+        title: 'Article',
         icon: 'assets/images/header/ico-home.png',
         iconActive: 'assets/images/header/ico-home--active.png',
-        routerLink: '/root/home',
-        accessRight: UserPrivileges.CONTACT_LIST
+        routerLink: '/root/home'
     },
     {
-        title: 'Autre',
+        title: 'Vente',
         icon: 'assets/images/header/ico-admin.png',
         iconActive: 'assets/images/header/ico-admin--active.png',
-        routerLink: '/root/admin',
-        accessRight: UserPrivileges.CONTACT_LIST
+        routerLink: '/root/admin'
     },
     {
         title: 'Administration',
         icon: 'assets/images/header/ico-param.png',
         iconActive: 'assets/images/header/ico-param--active.png',
-        routerLink: '/root/access-management',
-        accessRight: UserPrivileges.CONTACT_LIST
+        routerLink: '/root/access-management'
     }
 ];
