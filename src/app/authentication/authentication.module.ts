639b53c5c2a52d977dab5ef6b505d16e58ea21db
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 18:36:16 +0300

    [AUTHENTICATION] Remove mock service #master

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -2,12 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EffectsModule } from '@ngrx/effects';
-import { environment } from 'src/environments/environment';
 import { SharedModule } from '../shared/shared.module';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { AuthenticationRootComponent } from './containers/authentication-root/authentication-root.component';
-import { AuthenticationMockService } from './services/authentication-mock.service';
 import { AuthenticationService } from './services/authentication.service';
 import { AuthenticationEffects } from './store/effects/authentication.effects';
 
@@ -20,11 +18,6 @@ import { AuthenticationEffects } from './store/effects/authentication.effects';
         SharedModule
     ],
     declarations: [AuthenticationRootComponent, AuthenticationComponent],
-    providers: [
-        {
-            provide: AuthenticationService,
-            useClass: environment.mock ? AuthenticationMockService : AuthenticationService
-        }
-    ]
+    providers: [AuthenticationService]
 })
 export class AuthenticationModule {}
