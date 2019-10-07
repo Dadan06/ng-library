c7e055448970a4df49de9e37fe3bf236b0d27df5
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 20:07:47 +0300

    [PRODUCT] Add constants #master

---
+++ src/app/product/constants/product.constants.ts
@@ -0,0 +1,18 @@
+import { ProductCriteria } from '../types/product-criteria.interface';
+import { Product } from '../types/product.interface';
+
+export const PRODUCT_DEFAULT_CRITERIA: ProductCriteria = {
+    page: { page: 1, pageSize: 15 },
+    search: ''
+};
+
+export const EMPTY_PRODUCT_MODEL: Product = {
+    _id: null,
+    name: null,
+    costPrice: 0,
+    sellingPrice: 0
+};
+
+export const PRODUCT_API_ROUTE = 'product';
+
+export const PRODUCT_BASE_ROUTE = '/root/home/product';
