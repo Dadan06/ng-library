9a3ba649be17a7fca1f68b4c669897671513b01f
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 20:38:57 +0300

    [PRODUCT] Module update #master

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,4 +1,41 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserPrivileges } from '../role/constants/privilege.constants';
+import { PrivilegeGuard } from '../root/guards/privileges.guard';
+import { ProductFormRootComponent } from './containers/product-form-root/product-form-root.component';
+import { ProductListRootComponent } from './containers/product-list-root/product-list-root.component';
 
-@NgModule()
+const routes: Routes = [
+    {
+        path: '',
+        component: ProductListRootComponent,
+        canActivate: [PrivilegeGuard],
+        data: { privilege: UserPrivileges.VIEW_PRODUCT },
+        children: [
+            {
+                path: 'detail/:supplierId',
+                component: ProductFormRootComponent,
+                canActivate: [PrivilegeGuard],
+                data: { privilege: UserPrivileges.VIEW_PRODUCT }
+            },
+            {
+                path: 'edit/:supplierId',
+                component: ProductFormRootComponent,
+                canActivate: [PrivilegeGuard],
+                data: { privilege: UserPrivileges.EDIT_PRODUCT }
+            },
+            {
+                path: 'new',
+                component: ProductFormRootComponent,
+                canActivate: [PrivilegeGuard],
+                data: { privilege: UserPrivileges.CREATE_PRODUCT }
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
 export class ProductRoutingModule {}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { ModalModule } from 'angular-custom-modal';
+import { SharedModule } from '../shared/shared.module';
 import { ProductFormComponent } from './components/product-form/product-form.component';
-import { ProductListRootComponent } from './containers/product-list-root/product-list-root.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductFormRootComponent } from './containers/product-form-root/product-form-root.component';
+import { ProductListRootComponent } from './containers/product-list-root/product-list-root.component';
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductService } from './services/product.service';
+import { ProductRouterEffects } from './store/effects/product-router.effects';
+import { ProductEffects } from './store/effects/product.effects';
+import { productReducer } from './store/reducers/product.reducers';
 
 @NgModule({
-  declarations: [ProductListComponent, ProductFormComponent, ProductListRootComponent, ProductFormRootComponent],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        ProductListComponent,
+        ProductFormComponent,
+        ProductListRootComponent,
+        ProductFormRootComponent
+    ],
+    imports: [
+        CommonModule,
+        ProductRoutingModule,
+        SharedModule,
+        ReactiveFormsModule,
+        StoreModule.forFeature('product', productReducer),
+        EffectsModule.forFeature([ProductEffects, ProductRouterEffects]),
+        ModalModule
+    ],
+    providers: [ProductService]
 })
-export class ProductModule { }
+export class ProductModule {}
