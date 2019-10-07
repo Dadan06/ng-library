004252f380e1f0aa216cbf95ed8c083913753e9b
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 21:00:43 +0300

    Get current url

--- src/app/core/store/selectors/router.selectors.ts
+++ src/app/core/store/selectors/router.selectors.ts
@@ -1,5 +1,10 @@
 import { RouterReducerState } from '@ngrx/router-store';
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AppRouterState } from '../reducers/router.reducers';
 
 export const getRouterState = createFeatureSelector<RouterReducerState<AppRouterState>>('router');
+
+export const getUrl = createSelector<AppRouterState, RouterReducerState<AppRouterState>, string>(
+    getRouterState,
+    router => router.state.url
+);
