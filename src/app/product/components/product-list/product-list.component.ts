70a292e7df15355fea5b25134bf3af4a92891167
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 19:13:49 +0300

    [PRODUCT] List component #master

--- src/app/product/components/product-list/product-list.component.ts
+++ src/app/product/components/product-list/product-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../types/product.interface';
 
 @Component({
     selector: 'app-product-list',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent implements OnInit {
-    constructor() {
-        /** */
-    }
-
-    ngOnInit() {
-        /** */
-    }
+export class ProductListComponent {
+    @Input() products: Product[];
+    @Input() currentProduct: Product;
+    @Input() editEnabled = true;
+    @Input() deleteEnabled = true;
+    @Output() view: EventEmitter<Product> = new EventEmitter<Product>();
+    @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+    @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
 }
