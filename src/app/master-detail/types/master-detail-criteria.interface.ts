24a02c86070438dadaf49879f509710dd5273112
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 15:00:33 +0300

    Detail MASTER DETAIL #master

--- src/app/master-detail/types/master-detail-criteria.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from 'src/app/shared/types/page.interface';
-
-export interface MasterDetailCriteria {
-    page: Page;
-    search: string;
-}
--- src/app/master-detail/types/master-detail-model.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface MasterDetailModel {
-    _id: string;
-    prop1: string;
-    prop2: string;
-    prop3: string;
-    prop4: string;
-}
