b922d87e53180c5261e7b1d1bd45632e5ed8eb33
= <ismael.fidimalala@dreamslab.fr>
2019-10-08 16:57:58 +0300

    [AUTHENTICATION] Required field validator #master

--- src/app/authentication/components/authentication/authentication.component.ts
+++ src/app/authentication/components/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { markFormAsTouchedAndDirty } from 'src/app/shared/utils/form.utils';
 import { Credentials } from '../../types/credentials.interface';
 
 @Component({
@@ -18,10 +19,14 @@ export class AuthenticationComponent implements OnInit {
         this.form = this.initForm();
     }
 
+    onSubmit() {
+        this.form.valid ? this.logIn.emit(this.form.value) : markFormAsTouchedAndDirty(this.form);
+    }
+
     private initForm(): FormGroup {
         return this.formBuilder.group({
-            login: ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required]
+            login: [null, Validators.required],
+            password: [null, Validators.required]
         });
     }
 }
