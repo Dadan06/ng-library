da0797fe1ac25df352804be593b9d33b11dffdb8
= <ismael.fidimalala@dreamslab.fr>
2019-10-17 06:20:28 +0300

    [SALE] Filter actions #master

--- src/app/sale/containers/sale-root/sale-root.component.ts
+++ src/app/sale/containers/sale-root/sale-root.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
+import * as cloneDeep from 'lodash/cloneDeep';
 import { Observable } from 'rxjs';
+import { PRODUCT_DEFAULT_CRITERIA } from 'src/app/product/constants/product.constants';
+import { LoadProducts } from 'src/app/product/store/actions/product.actions';
 import { ProductState } from 'src/app/product/store/reducers/product.reducers';
 import {
     getProducts,
     getProductsLoading,
     getProductsTotalItems
 } from 'src/app/product/store/selectors/product.selectors';
+import { ProductCriteria } from 'src/app/product/types/product-criteria.interface';
 import { Product } from 'src/app/product/types/product.interface';
+import { Page } from 'src/app/shared/types/page.interface';
 
 @Component({
     selector: 'app-sale-root',
@@ -18,6 +23,7 @@ export class SaleRootComponent implements OnInit {
     products$: Observable<Product[]>;
     productsLoading$: Observable<boolean>;
     totalItems$: Observable<number>;
+    productCriteria: ProductCriteria = cloneDeep(PRODUCT_DEFAULT_CRITERIA);
 
     constructor(private productStore: Store<ProductState>) {
         /** */
@@ -27,5 +33,15 @@ export class SaleRootComponent implements OnInit {
         this.products$ = this.productStore.pipe(select(getProducts));
         this.productsLoading$ = this.productStore.pipe(select(getProductsLoading));
         this.totalItems$ = this.productStore.pipe(select(getProductsTotalItems));
+    }
+
+    onSearch(search: string) {
+        this.productCriteria.search = search;
+        this.productStore.dispatch(new LoadProducts({ ...this.productCriteria }));
+    }
+
+    onPaginate(page: Page) {
+        this.productCriteria.page = page;
+        this.productStore.dispatch(new LoadProducts({ ...this.productCriteria }));
     }
 }
