1224a81a6f73852692621d7421bbe0ffb578fbd3
= <ismael.fidimalala@dreamslab.fr>
2019-10-18 16:03:30 +0300

    Menu renaming #develop

--- src/app/root/constants/header-menu.constant.ts
+++ src/app/root/constants/header-menu.constant.ts
@@ -2,19 +2,19 @@ import { Menu } from '../types/menu.interface';
 
 export const HEADER_MENU: Menu[] = [
     {
-        title: 'Article',
+        title: 'Gestion des articles',
         icon: 'assets/images/header/ico-home.png',
         iconActive: 'assets/images/header/ico-home--active.png',
         routerLink: '/root/home'
     },
     {
-        title: 'Vente',
+        title: 'Gestion des ventes',
         icon: 'assets/images/header/ico-admin.png',
         iconActive: 'assets/images/header/ico-admin--active.png',
         routerLink: '/root/admin'
     },
     {
-        title: 'Administration',
+        title: 'Administration accès',
         icon: 'assets/images/header/ico-param.png',
         iconActive: 'assets/images/header/ico-param--active.png',
         routerLink: '/root/access-management'
