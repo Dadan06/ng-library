6e1a936da306d14fab025c3a32c38d2edec2723f
MIKA <delmickr@gmail.com>
2019-11-09 09:50:32 +0300

    [SALE-MONITORING] Generate feature #master

---
+++ src/app/sale-monitoring/containers/sale-detail-root/sale-detail-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sale-detail-root',
+    templateUrl: './sale-detail-root.component.html',
+    styleUrls: ['./sale-detail-root.component.scss']
+})
+export class SaleDetailRootComponent {
+
+}
---
+++ src/app/sale-monitoring/containers/sale-root/sale-root.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sale-root',
+    templateUrl: './sale-root.component.html',
+    styleUrls: ['./sale-root.component.scss']
+})
+export class SaleRootComponent {
+
+}
