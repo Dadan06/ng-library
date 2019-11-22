812b7d0743def35920f213c11fce2176f92714bb
FIDIMALALA <chiyangchiying@gmail.com>
2019-11-22 21:35:21 +0400

    [SALE] Refactoring #master

--- src/app/sale/constants/sale.constant.ts
+++ src/app/sale/constants/sale.constant.ts
@@ -1,3 +1,5 @@
 export const SALE_API_ROUTE = 'sale';
 
+export const SALE_ITEM_API_ROUTE = 'sale-item';
+
 export const SALE_BASE_ROUTE = '/root/admin/sale';
--- src/app/sale/services/sale.service.ts
+++ src/app/sale/services/sale.service.ts
@@ -6,6 +6,7 @@ import { Product } from 'src/app/product/types/product.interface';
 import { ApiResponse } from 'src/app/shared/types/api-response.interface';
 import { User } from 'src/app/user/types/user.interface';
 import { environment } from 'src/environments/environment';
+import { SALE_API_ROUTE, SALE_ITEM_API_ROUTE } from '../constants/sale.constant';
 import { SaleItem } from '../types/sale-item.interface';
 import { Sale } from '../types/sale.interface';
 
@@ -15,41 +16,46 @@ export class SaleService {
 
     createSale(user: User): Observable<Sale> {
         return this.http
-            .post(`${environment.apiBaseUrl}/sale`, user)
+            .post(`${environment.apiBaseUrl}/${SALE_API_ROUTE}`, user)
             .pipe(map((response: ApiResponse) => response.data as Sale));
     }
 
     addProduct(saleId: string, product: Product): Observable<SaleItem> {
         return this.http
-            .post(`${environment.apiBaseUrl}/sale/${saleId}/add-product`, product)
+            .post(`${environment.apiBaseUrl}/${SALE_API_ROUTE}/${saleId}/add-product`, product)
             .pipe(map((response: ApiResponse) => response.data as SaleItem));
     }
 
     deleteSaleItem(saleItem: SaleItem): Observable<SaleItem> {
         return this.http
-            .put(`${environment.apiBaseUrl}/sale-item/${saleItem._id}/delete`, saleItem)
+            .put(
+                `${environment.apiBaseUrl}/${SALE_ITEM_API_ROUTE}/${saleItem._id}/delete`,
+                saleItem
+            )
             .pipe(map((response: ApiResponse) => response.data as SaleItem));
     }
 
     cancelSale(saleId: string): Observable<void> {
         return this.http
-            .post(`${environment.apiBaseUrl}/sale/${saleId}/cancel`, {})
+            .post(`${environment.apiBaseUrl}/${SALE_API_ROUTE}/${saleId}/cancel`, {})
             .pipe(map(() => null));
     }
 
     incrementQty(saleItem: SaleItem): Observable<SaleItem> {
         return this.http
-            .post(`${environment.apiBaseUrl}/sale-item/increment-qty`, saleItem)
+            .post(`${environment.apiBaseUrl}/${SALE_ITEM_API_ROUTE}/increment-qty`, saleItem)
             .pipe(map((response: ApiResponse) => response.data as SaleItem));
     }
 
     decrementQty(saleItem: SaleItem): Observable<SaleItem> {
         return this.http
-            .post(`${environment.apiBaseUrl}/sale-item/decrement-qty`, saleItem)
+            .post(`${environment.apiBaseUrl}/${SALE_ITEM_API_ROUTE}/decrement-qty`, saleItem)
             .pipe(map((response: ApiResponse) => response.data as SaleItem));
     }
 
     saveSale(sale: Sale): Observable<void> {
-        return this.http.post(`${environment.apiBaseUrl}/payment`, sale).pipe(map(() => null));
+        return this.http
+            .post(`${environment.apiBaseUrl}/${SALE_API_ROUTE}/save`, sale)
+            .pipe(map(() => null));
     }
 }
