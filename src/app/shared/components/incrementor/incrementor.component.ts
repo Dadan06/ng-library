bc8ac5771ff58cc0639955c73e4335c8feab3a21
FIDIMALALA <chiyangchiying@gmail.com>
2019-11-23 09:57:30 +0400

    [INCREMENTOR] Allow manual value #master

--- src/app/shared/components/incrementor/incrementor.component.ts
+++ src/app/shared/components/incrementor/incrementor.component.ts
@@ -6,9 +6,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
     styleUrls: ['./incrementor.component.scss']
 })
 export class IncrementorComponent {
-    @Input() value = 0;
+    @Input() value = 1;
     @Input() disabled = false;
 
     @Output() increment: EventEmitter<void> = new EventEmitter();
     @Output() decrement: EventEmitter<void> = new EventEmitter();
+    @Output() changeValue: EventEmitter<number> = new EventEmitter();
+
+    currentValues = [this.value];
+    private digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+    onKeyUp(event: KeyboardEvent) {
+        if (this.digits.indexOf(event.key) > -1) {
+            this.currentValues.push(+event.key);
+        }
+        this.changeValue.emit(+this.currentValues.join(''));
+    }
 }
