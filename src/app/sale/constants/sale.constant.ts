a6c9dcfdc9aa38221b9c495217be8f21e75c4575
FIDIMALALA <chiyangchiying@gmail.com>
2019-11-30 14:13:34 +0400

    [SALE] Add new constant #master

--- src/app/sale/constants/sale.constant.ts
+++ src/app/sale/constants/sale.constant.ts
@@ -1,5 +1,12 @@
+import { SaleType } from '../types/sale.interface';
+
 export const SALE_API_ROUTE = 'sale';
 
 export const SALE_ITEM_API_ROUTE = 'sale-item';
 
 export const SALE_BASE_ROUTE = '/root/admin/sale';
+
+export const SALE_TYPE_LABELS = {
+    [SaleType.CONSIGNATION]: 'Consignation',
+    [SaleType.DIRECT_SALE]: 'Direct'
+};
