7af1291a7355118a2b144b97d8c000c9946e82e1
FIDIMALALA <chiyangchiying@gmail.com>
2019-11-30 14:20:10 +0400

    [SALE MONITORING] Add new constant #master

--- src/app/sale-monitoring/constants/sale-monitoring.constant.ts
+++ src/app/sale-monitoring/constants/sale-monitoring.constant.ts
@@ -5,15 +5,17 @@ export const SALE_DEFAULT_CRITERIA: SaleCriteria = {
     search: '',
     filter: {
         from: ((d = new Date()) => `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()} 00:00`)(),
-        to: ((d = new Date()) => `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()} 00:00`)(),
+        to: ((d = new Date()) => `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()} 23:59`)(),
         sellers: null,
+        clients: null,
         types: null
     }
 };
 
 export const SALE_FILTER_CATEGORY_LABELS: Record<string, string> = {
     sellers: 'Vendeurs',
-    types: 'Types de vente'
+    types: 'Types de vente',
+    clients: 'clients'
 };
 
 export const SALE_FILTER_ITEM_LABELS: Record<string, Object> = {};
